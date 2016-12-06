f05d007d79951c3bc9b43db098af19cecc749149
Jon Rowe <hello@jonrowe.co.uk>
2016-12-06 15:51:22 +1100

    publish build

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BarApp from './BarApp';
-import './index.css';
-
-ReactDOM.render(
-  <BarApp />,
-  document.getElementById('root')
-);
